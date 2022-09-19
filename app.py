87dd02afecd6e810e0eef936a740d867be0d277d
sm20000618 <47664294+sm20000618@users.noreply.github.com>
2022-09-19 18:42:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request, redirect
 import MySQLdb
 app = Flask(__name__)
 
-# Configure database
+#Database configuration 
 
-conn=MySQLdb.connect(
+connectiondb=MySQLdb.connect(
         host='localhost',
         user='root',
         password = 'Honeyprema@0218',
@@ -20,33 +20,38 @@ def index():
         emplDetails = request.form
         emp_id = emplDetails['emp_id']
         emp_name = emplDetails['emp_name']
-        emp_dept = emplDetails['emp_dept']
-        # cur = mysql.connection.cursor()
+        emp_department = emplDetails['emp_department']
         #connection eshtablished by using cursor()
-        cur = conn.cursor()
-        cur.execute("INSERT INTO new_table(emp_id, emp_name, emp_dept) VALUES(%s, %s, %s)",(emp_id, emp_name, emp_dept))
-        conn.commit()
-
-        cur.close()
+        cursor = connectiondb.cursor()
+        cursor.execute("INSERT INTO new_table(emp_id, emp_name, emp_department) VALUES(%s, %s, %s)",(emp_id, emp_name, emp_department))
+        connectiondb.commit() cursor.close()
         return redirect('/users')
     return render_template('index.html')
 #get the count along with department name
 #get method
 @app.route('/users')
 def users():
-    cur = conn.cursor()
-    countdepartment= cur.execute("SELECT emp_dept, COUNT(*) FROM new_table GROUP BY emp_dept ")
+    cursor = connectiondb.cursor()
+    countdepartment= cursor.execute("SELECT emp_department, COUNT(*) FROM new_table GROUP BY emp_department ")
     if countdepartment > 0:
-        Details= cur.fetchall()
+        Details= cursor.fetchall()
         return render_template('users.html',Details=Details)
 #get the name  which is matching with the given pattern
 #get method
 @app.route('/find')
 def findEmployee():
-    cur=conn.cursor()
-    findemp=cur.execute("SELECT emp_name FROM new_table where emp_name like '%na'")
-    Details=cur.fetchall()
+    cursor=connectiondb.cursor()
+    findemp=cursor.execute("SELECT emp_name FROM new_table where emp_name like '%na'")
+    Details=cursor.fetchall()
     return render_template('users.html',Details=Details)
+#get methods
+@app.route('/casesensitive)
+def caseSensitive():
+    cursor=connectiondb.cursor()
+    findcs=cursor.execute("SELECT emp_name, COUNT(*) FROM new_table GROUP BY emp_name")
+    Details=cursor.fetchall()
+    return render_template('users.html',Details=Details)
+
 #main method
 if __name__ == '__main__':
     app.run(debug=True)
